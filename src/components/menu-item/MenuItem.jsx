49ca1a02da63d950f7368067de392b21ab4db803
MrAnAcondA <shahzebjadoon0@gmail.com>
2022-02-09 18:48:27 +0500

    broken home page into smaller components and finshed styling home component

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-
-const MenuItem = ({ title }) => {
+import './menuItem.styles.scss';
+const MenuItem = ({ title, imageUrl, size, id, linkUrl }) => {
+  console.log(title);
   return (
-    <div className='menu-item'>
+    <div className={`menu-item ${size}`}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
       <div className='content'>
         <h2 className='title'>{title}</h2>
         <span className='subtitle'>Shop Now</span>
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import MenuItem from '../../components/menu-item/MenuItem';
+import Directory from '../../components/directory/Directory';
 import './homepage.styles.scss';
 
 const HomePage = () => {
   return (
     <div className='homepage'>
-      <div className='directory-menu'>
-        <MenuItem title='Hat' />
-        <MenuItem title='Jeans' />
-        <MenuItem title='Shirt' />
-        <MenuItem title='Shoes' />
-      </div>
+      <Directory />
     </div>
   );
 };
